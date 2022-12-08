71709c1bb85d3b944ef5a89d4fc99fcf45b0979f
miyamoto <miritoM3810@outlook.jp>
2022-12-08 19:01:02 +0900

    mdを読み込む

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="marked" v-html="tttt"></div>
+</template> 
+
+<script lang="ts">
+  import {marked} from 'marked';
+
+
+  const mdText = require("/markdowns/test.md");
+  console.log(mdText)
+  export default {
+    setup(){
+        const tttt = marked(mdText.default);
+        return {
+            tttt
+        }
+    }
+  };
+</script>
